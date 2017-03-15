c8c7df82449af93a99f511898d5d22df3a2717e7
Miriam Tocino <miriam.tocino@gmail.com>
2017-03-15 14:20:27 +0100

    Through Association: Views
    - Create Registration form
    - Update RegistrationsController
    - Add methods `set_status` and `set_total_price` to Registration model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,14 +2,18 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @registration = current_user.registrations.create(registration_params)
+    @registration = current_user.registrations.new(registration_params)
+    @registration.event_id = params[:event_id]
+    @registration.set_total_price
+    @registration.set_status
+    @registration.save
 
     redirect_to @registration.event, notice: "Thank you for registering!"
   end
 
   private
 
-    def booking_params
-      params.require(:registration).permit(:price, :guests_count, :event_id)
+    def registration_params
+      params.require(:registration).permit(:guests_count)
     end
 end
