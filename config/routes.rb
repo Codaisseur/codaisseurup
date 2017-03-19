fbf96ba29f9281355a094f27641b3129d0aaa1d2
Miriam Tocino <miriam.tocino@gmail.com>
2017-03-19 12:12:33 +0100

    Add API registration endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   resources :photos
 
   namespace :api do
-    resources :events
+    resources :events do
+      resources :registrations, only: [:create, :update, :destroy]
+    end
   end
 end
