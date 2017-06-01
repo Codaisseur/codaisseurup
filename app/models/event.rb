19a0b2118aefbb763d5cb134bc93add15b56dadd
Wouter de Vos <wouter@codaisseur.com>
2017-06-01 15:22:13 +0200

    Example event class methods and tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,25 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
 
+  scope :published, ->{ where(active: true) }
+
   def bargain?
     price < 30
   end
 
+  def self.on_date(date)
+    where("? BETWEEN starts_at AND ends_at", date)
+  end
+
+  def self.starts_on(date)
+    where("starts_at BETWEEN ? AND ?", date.beginning_of_day, date.end_of_day)
+  end
+
   def self.order_by_price
     order(:price)
+  end
+
+  def self.by_name
+    order(:name)
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -76,4 +76,54 @@ RSpec.describe Event, type: :model do
       expect(event.guests).to include(guest_user)
     end
   end
+
+  describe ".by_name returns events ordered by their name in alphabetical order" do
+    let!(:b_event) { create :event, name: "Bs" }
+    let!(:c_event) { create :event, name: "Cmon" }
+    let!(:a_event) { create :event, name: "Aha" }
+
+    it { expect(Event.by_name.to_a).to eq [a_event, b_event, c_event] }
+  end
+
+  describe ".published returns only published events" do
+    let!(:pub_events) { create_list :event, 3, active: true }
+    let!(:unpub_events) { create_list :event, 2, active: false }
+
+    it { expect(Event.published).not_to match_array unpub_events }
+    it { expect(Event.published).to match_array pub_events }
+  end
+
+  describe "date magic" do
+    let!(:long_event) { create :event, starts_at: "2017-06-01", ends_at: "2017-06-04" }
+    let!(:short_event) { create :event, starts_at: "2017-06-02", ends_at: "2017-06-02" }
+
+    let(:june1) { Date.parse("2017-06-01") }
+    let(:june2) { Date.parse("2017-06-02") }
+    let(:june3) { Date.parse("2017-06-03") }
+    let(:june4) { Date.parse("2017-06-04") }
+    let(:june5) { Date.parse("2017-06-05") }
+    let(:last_year) { Date.parse("2016-06-03") }
+    let(:crazy_time) { DateTime.parse("2017-06-02 11:45:59") }
+
+    describe ".on_date returns events planned on that date" do
+      it { expect(Event.on_date(june1)).to include(long_event) }
+      it { expect(Event.on_date(june1)).not_to include(short_event) }
+      it { expect(Event.on_date(june2)).to include(long_event) }
+      it { expect(Event.on_date(june2)).to include(short_event) }
+      it { expect(Event.on_date(june3)).to include(long_event) }
+      it { expect(Event.on_date(june3)).not_to include(short_event) }
+      it { expect(Event.on_date(june4)).to include(long_event) }
+      it { expect(Event.on_date(june4)).not_to include(short_event) }
+      it { expect(Event.on_date(june5)).not_to include(long_event) }
+      it { expect(Event.on_date(june5)).not_to include(short_event) }
+      it { expect(Event.on_date(last_year)).not_to include(long_event) }
+      it { expect(Event.on_date(last_year)).not_to include(short_event) }
+    end
+
+    describe ".starts_on returns events on that date" do
+      it { expect(Event.starts_on(june1)).to include(long_event) }
+      it { expect(Event.starts_on(june1)).not_to include(short_event) }
+      it { expect(Event.starts_on(crazy_time)).to include(short_event) }
+    end
+  end
 end
