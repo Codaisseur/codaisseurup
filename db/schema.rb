74fe4c3c1388d510e44edbd2deb134ed52faeda3
Arno Fleming <arno.fleming@gmail.com>
2017-11-08 17:13:11 +0100

    Update bundle, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,61 +15,61 @@ ActiveRecord::Schema.define(version: 20170311091310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "categories_events", id: false, force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "event_id",    null: false
-    t.index ["category_id", "event_id"], name: "index_categories_events_on_category_id_and_event_id", using: :btree
-    t.index ["event_id", "category_id"], name: "index_categories_events_on_event_id_and_category_id", using: :btree
+    t.bigint "category_id", null: false
+    t.bigint "event_id", null: false
+    t.index ["category_id", "event_id"], name: "index_categories_events_on_category_id_and_event_id"
+    t.index ["event_id", "category_id"], name: "index_categories_events_on_event_id_and_category_id"
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "location"
-    t.boolean  "includes_food"
-    t.boolean  "includes_drinks"
-    t.decimal  "price"
+  create_table "events", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.boolean "includes_food"
+    t.boolean "includes_drinks"
+    t.decimal "price"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.integer  "capacity"
-    t.boolean  "active"
-    t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.text     "bio"
-    t.integer  "user_id"
+    t.integer "capacity"
+    t.boolean "active"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "profiles", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "events", "users"
